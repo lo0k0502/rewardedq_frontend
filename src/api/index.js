fca1988782e5296078b645d20f37e8048cd07772
lo0k0502 <thomas85514@gmail.com>
2021-07-30 13:53:29 +0800

    fix backend url

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8000/api' });
+const API = axios.create({ baseURL: 'https://admdmt.ccu.edu.tw/rewardedq_backend/index.php' });
 
 export const storeUser = (payload) => API.post('/user/store', payload, { headers:{ 'Content-Type': 'application/json' } });
